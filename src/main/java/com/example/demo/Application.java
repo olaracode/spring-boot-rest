21363646d2f0f284bf819f4e89b071837a427139
Octavio Lara <octavioasapchi@gmail.com>
2024-11-13 11:35:59 -0300

    ✨ create Run class/record and first instance using Bean

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -1,16 +1,28 @@
 package com.example.demo;
 
+import com.example.demo.run.Location;
+import com.example.demo.run.Run;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 
+import java.time.temporal.ChronoUnit;
+import java.time.LocalDateTime;
 @SpringBootApplication
 public class Application {
 	private static final Logger log = LoggerFactory.getLogger(Application.class);
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
-		log.info("Applicatiaasdasdsdon");
+	}
+	@Bean
+	CommandLineRunner runner(){
+		return args -> {
+			Run run = new Run(1,"First run",  LocalDateTime.now(),  LocalDateTime.now().plus(1, ChronoUnit.HOURS),5, Location.OUTDOOR);
+			log.info("RUN: " + run);
+		};
 	}
 }
